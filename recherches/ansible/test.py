378c7bee90b3e19f5b88a48baea1ebbe91d37eb3
RonanLetertre <ronan.letertre27@gmail.com>
2024-02-14 16:30:42 +0100

    Update test.py
    exemple d'un script python avec un fichier yaml et jinja

--- recherches/ansible/test.py
+++ recherches/ansible/test.py
@@ -1,8 +1,15 @@
 #test d'un script python
 import yaml
+from jinja2 import Template
 from pprint import pprint as p
 
+#lire le fichier yaml
 with open("exemple.yml") as file:
     info_router = yaml.safe_load(file)
 
-p(info_router)
+#lire la template jinja
+with open("exemple.j2") as file:
+    template = Template(file.read())
+
+
+#p(info_router)
